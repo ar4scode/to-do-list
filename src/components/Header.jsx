a267870ff3f23fc507cf20e35355f750a136dfb5
Ar4sCode <ar4sazizi@gmail.com>
2024-11-04 20:24:29 +0330

    Add essential files and Create header for webpage

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return(
+    <>
+    <header>
+      <p className="name">Ar<span>4</span>sCode</p>
+      <p className="project-name"><span>To Do </span>List</p>
+    </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function MainContent() {
+  return (
+    <div className="container">
+      <div className="checkbox-container">
+        <input type="checkbox" name="" id="" />
+        <p>To Do List</p>
+      </div>
+    </div>
+  )
+}
+
+export default MainContent
