5146afd40a45cce2a875bd8b101eb8d8cad9f954
Ar4sCode <ar4sazizi@gmail.com>
2024-11-06 22:07:11 +0330

    Create Footer and style it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header"
 import MainContent from "./MainContent"
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <MainContent/>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="text-content">
+        <p>Author:<span> Ar4sCode</span></p>
+        <p className="project-info">Look for more Projects: Follow My <a href="https://github.com/ar4scode">Github</a></p>
+      </div>
+
+      <div className="logo-container">
+        <a href="https://github.com/ar4scode"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/ar4scode"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.instagram.com/_arasazizi/"><i class="fa-brands fa-instagram"></i></a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
