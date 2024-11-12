6d76745e469cc5cf3f3adf11adb4f17f96d8be70
Ar4sCode <ar4sazizi@gmail.com>
2024-11-12 11:28:13 +0330

    Change App component to class-based component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,26 @@
 import React from "react"
 import TodoItem from "./Todoitem"
 import { todoData } from "../data/todoData"
+import { Component } from "react"
 
-function MainContent() {
-  const todoList = todoData.map((item) => <TodoItem key={item.id} data={item} />)
-  
-  return (
-    <div className="container">
-      {todoList}
-    </div>
-  )
+class MainContent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos : todoData
+    }
+  }
+
+  render() {
+    const todoList = this.state.todos.map((item) => <TodoItem key={item.id} data={item} />)
+    
+    return (
+      <div className="container">
+        {todoList}
+      </div>
+    )
+
+  }
 }
 
 export default MainContent
