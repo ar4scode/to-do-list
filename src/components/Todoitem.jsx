d12dc26e0c60b39fdfd52c2974adee12235dbb81
Ar4sCode <ar4sazizi@gmail.com>
2024-12-03 21:44:07 +0330

    Make condition rendering for add new style when task is completed

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,8 +1,15 @@
 const TodoItem = (props) => {
+  const completedStyle = {
+    fontSize: "1.2rem",
+    textDecoration: "line-through",
+    color: "#8b8787",
+    fontStyle: "italic"
+  }
+  
   return (
     <div className="checkbox-container">
         <input type="checkbox" name="" id="" checked={props.data.completed} onChange={() => props.handleChange(props.data.id)}/>
-        <p>{props.data.text}</p>
+        <p style={props.data.completed ? completedStyle : null}>{props.data.text}</p>
     </div>
   )
 }
