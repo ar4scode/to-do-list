7e9a1aee17067d769474bf7f7200fa971f80aac6
Ar4sCode <ar4sazizi@gmail.com>
2025-01-20 14:36:54 +0330

    Create form using Formik and add validation using Yup

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import TodoItem from "./Todoitem";
 import { todoData } from "../data/todoData";
+import {useFormik} from "formik"
+import * as Yup from "yup"
 
 const MainContent = () => {
   const [todos, setTodos] = useState(todoData);
   const [isCompleted, setIsCompleted] = useState(false)
+
+  const formik = useFormik({
+    initialValues: {
+      todoTask: ""
+    },
+    validationSchema: Yup.object({
+      todoTask: Yup.string()
+        .required("Enter New Task")
+    })
+  })
 
   const handleChange = (id) => {
     setTodos((prevTodos) =>
@@ -18,7 +30,22 @@ const MainContent = () => {
     <TodoItem key={item.id} data={item} handleChange={handleChange} />
   ));
 
-  return <div className="container">{todoList}</div>;
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          value={formik.values.todoTask}
+          placeholder="New Task"
+        />
+        <button type="submit">Add</button>
+      </form>
+      {formik.errors.todoTask ? (
+          <div className="input-error">{formik.errors.todoTask}</div>
+        ) : null}
+      <div className="container">{todoList}</div>
+    </>
+  ) 
 };
 
 export default MainContent;
